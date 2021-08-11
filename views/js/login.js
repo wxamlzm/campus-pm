7ca35fb181609acca392cbebde8e9adadf4e7fc4
wxamlzm <zd5024@163.com>
2021-08-11 19:25:38 +0800

    UPDATE:对象封装初步完成

--- views/js/login.js
+++ views/js/login.js
@@ -1,16 +1,10 @@
-// 登录部分与注册部分切换
-// 默认显示登录按钮，点击注册按钮后触发
-// 尝试通过冒泡来进行实现
+// 获取外层父级作为事件委托对象
 var tabBox = document.querySelector('[data-tabBox]');
-// 利用事件冒泡绑定两个a元素的事件处理函数
-// 同时通过识别获取的元素对象情况，来决定切换
+// 对父级绑定事件处理函数
 tabBox.onclick = e => {
     var div = document.querySelector('.login-panel');
-
-    // 设置两个面板的对象
-    // 获取目标元素对象
-    // 获取需要调整的整个dom对象的
-    var loginPanelByPwd = {
+    // 创建整个panel的虚拟dom对象
+    var loginPanelDom = {
         'login-title': {
             aByPwd: tabBox.querySelector(':first-child'),
             aByPhone: tabBox.querySelector(':last-child')
@@ -20,13 +14,10 @@ tabBox.onclick = e => {
             inputPwd: div.querySelector('.login-input > :last-child')
         },
     }
-
-    // 设置需要修改的元素对象属性
-    // 两个面板永远设置相反的状态,用来控制
+    // 设置需要修改的元素对象属性的值
     var loginByPwd = {
         text: '请输入手机号/邮箱/用户名',
         pwd: '请输入密码',
-        status: 'true',
         link1: '忘记密码',
         link2: '免费注册'
     }
@@ -35,27 +26,23 @@ tabBox.onclick = e => {
         text:'请输入手机号',
         msg: '请输入短信验证码',
         btn: '获取验证码',
-        status: 'false',
         link2: '免费注册'
     }
+    // 选中两个tab中的两个a标签
+    var tabs = document.querySelectorAll('[data-tab]');
     // 点击目标元素后，首先设置当前目标元素状态为true，同时设置另一个目标元素状态为false
-
-    // 选中两个title中的两个a标签
-    var titles = document.querySelectorAll('[data-tab]');
-
     if ('A' === e.target.nodeName){
-        // 先重置两个a标签的状态为false，再设置目标元素状态为true
-        // 遍历类数组对象设置false重置
-        for (let title of titles){
-            title.dataset.status = false;
+        for (let tab of tabs){
+            tab.dataset.status = false;
         }
         e.target.dataset.status = true;
         // 通过判断a标签的值，设置对应的业务需求
-        if (loginPanel.aByPwd === e.target){
-            // 获取修改元素所需的dom对象
-            var input
-        }else if (loginPanel.aByPhone === e.target){
-
+        if (loginPanelDom['login-title'].aByPwd == e.target){
+            loginPanelDom['login-input'].inputTXT.placeholder = loginByPwd.text;
+            loginPanelDom['login-input'].inputPwd.placeholder = loginByPwd.pwd;
+        }else if (loginPanelDom['login-title'].aByPhone == e.target){
+            loginPanelDom['login-input'].inputTXT.placeholder = loginByMsg.text;
+            loginPanelDom['login-input'].inputPwd.placeholder = loginByMsg.msg;
         }
     }
 }
